2a134dac76934fb10aecd008989ddf41878925c8
blueberry <bluebbberry123@gmail.com>
2025-02-25 20:35:21 +0100

    add backend

--- backend/fungus-backend/src/index.ts
+++ backend/fungus-backend/src/index.ts
@@ -1,14 +1,12 @@
 import { serve } from "@hono/node-server";
 import { behindProxy } from "x-forwarded-fetch";
-import federation from "./federation.ts";
+import app from "./app.tsx";
 import "./logging.ts";
 
 serve(
   {
     port: 8000,
-    fetch: behindProxy(
-      (req) => federation.fetch(req, { contextData: undefined }),
-    ),
+    fetch: behindProxy(app.fetch.bind(app)),
   },
   (info) =>
     console.log("Server started at http://" + info.address + ":" + info.port)
